38162c781d66655eb4372ea0724b0ac4cfc0036d
Yasser Abdulah Ramadhan <yasseracer1@gmail.com>
2024-03-30 22:06:35 +0700

    Menghubungkan project ke github

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mobpro1-Assessment1"
+include(":app")
+ 
